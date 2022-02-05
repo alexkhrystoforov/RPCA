834ddbb3a500c27648e67810a6b6b44d0ff3b7da
Alex Khrystoforov <alexkhrystoforov@gmail.com>
2022-02-05 16:25:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# import numpy as np
-# import glob
-# import cv2
-#
-#
-# img_array = []
-# for filename in glob.glob('GroundtruthSeq/RawImages/*.bmp'):
-#     img = cv2.imread(filename)
-#     height, width, layers = img.shape
-#     size = (width, height)
-#     img_array.append(img)
-
-# out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-#
-# for i in range(len(img_array)):
-#     out.write(img_array[i])
-# out.release()
 import cv2
 import glob
 import re
@@ -32,7 +15,7 @@ for filename in sorted(glob.glob('GroundtruthSeq/RawImages/*.bmp') , key=numeric
     size = (width,height)
     img_array.append(img)
 
-# out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 out = cv2.VideoWriter('final_project.mp4', fourcc, 20.0, size)
 for i in range(len(img_array)):
